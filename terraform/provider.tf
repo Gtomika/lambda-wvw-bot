9a6bd622b24beca6d2ca129b40996a6d4d91d786
Gáspár Tamás <gaspar.tomi97@gmail.com>
2023-04-07 17:56:02 +0200

    Fix duration

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,7 +7,7 @@ provider "aws" {
   assume_role {
     role_arn = var.aws_terraform_role_arn
     external_id = var.aws_assume_role_external_id
-    duration = "3600" # seconds
+    duration = "1h"
     session_name = "TerraformSession"
   }
 
