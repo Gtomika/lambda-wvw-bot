2679bcd195bc218e307ddf599cfdcde1c7678e02
Gtomika <gaspar.tamas97@protonmail.com>
2024-03-08 15:51:19 +0100

    [ANNOUNCE]Upgraded wvw_daily and wvw_weekly commands to include WvW related Wizards Vault progress.

--- terraform/lambda_environment.tf
+++ terraform/lambda_environment.tf
@@ -267,7 +267,7 @@ locals {
     }
 
     WvwWeekly = {
-      policy    = data.aws_iam_policy_document.command_lambda_base_policies
+      policy    = data.aws_iam_policy_document.user_table_manager_lambda_policy
       variables = merge(local.common_variables, {
         GW2_USERS_TABLE_NAME = module.dynamodb_tables.gw2_users_table_name
       })
